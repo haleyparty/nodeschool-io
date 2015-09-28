6773380891aad5889a03fea985166ba43a8b5a31
Haley Bash <halebash@umich.edu>
2015-09-28 09:28:15 -0700

    Complete JavaScript review

---
+++ javascripting/function-arguments.js
@@ -0,0 +1,6 @@
+var math = function(arg1, arg2, arg3) {
+  result = arg2 * arg3 + arg1;
+  return result;
+}
+
+console.log(math(53, 61, 67));
---
+++ javascripting/looping-through-arrays.js
@@ -0,0 +1,7 @@
+var pets = ['cat', 'dog', 'rat'];
+
+for (pet in pets) {
+  pets[pet] += 's';
+}
+
+console.log(pets);
