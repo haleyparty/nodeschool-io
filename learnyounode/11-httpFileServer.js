2c0e6f2221c842a3ceb15f7b16934e01c073e1fa
Haley Bash <halebash@umich.edu>
2015-09-28 13:22:04 -0700

    Be a good web citizen and set file's content-type properly

--- learnyounode/11-httpFileServer.js
+++ learnyounode/11-httpFileServer.js
@@ -5,5 +5,6 @@ var port = process.argv[2];
 var file = process.argv[3];
 
 http.createServer(function callback(request, response) {
+  response.writeHead(200, {'content-type' : 'text/plain'})
   fs.createReadStream(file).pipe(response);
 }).listen(port);
