b552430010e9cdcb647fe63be228c3652ab49e68
Haley Bash <halebash@umich.edu>
2015-09-28 12:36:12 -0700

    Write http server that serves the same text file for each request it receives

---
+++ learnyounode/11-httpFileServer.js
@@ -0,0 +1,9 @@
+var fs = require('fs');
+var http = require('http');
+
+var port = process.argv[2];
+var file = process.argv[3];
+
+http.createServer(function callback(request, response) {
+  fs.createReadStream(file).pipe(response);
+}).listen(port);
