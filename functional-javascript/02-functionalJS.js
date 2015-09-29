4e8b0ef9314c2a2f0006b069d366bc4e8fa7ca4c
Haley Bash <halebash@umich.edu>
2015-09-28 17:59:30 -0700

    Refactor code to implement recursively

--- functional-javascript/02-functionalJS.js
+++ functional-javascript/02-functionalJS.js
@@ -1,6 +1,8 @@
 function repeat(operation, num) {
-  for (var i = 0; i < num; i++) {
-    operation();
+  if (num === 0) {
+    return operation();
+  } else {
+    return repeat(operation, num-1);
   }
 }
 
