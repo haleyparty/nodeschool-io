bbd65acf97b22f2c403d31627722b3d8a1623d86
Haley Bash <halebash@umich.edu>
2015-09-30 14:39:49 -0700

    If error, throw error

--- learnyoumongo/04-findLimit.js
+++ learnyoumongo/04-findLimit.js
@@ -4,6 +4,8 @@ var age = process.argv[2]
 var url = 'mongodb://localhost:27017/learnyoumongo'
 
 mongo.connect(url, function(err, db) {
+  if (err) throw err
+
   var parrots = db.collection('parrots')
   parrots.find({
     age: {
@@ -14,6 +16,8 @@ mongo.connect(url, function(err, db) {
     , age: 1
     , _id: 0
   }).toArray(function(err, documents) {
+    if (err) throw err
+
     console.log(documents)
     db.close()
   })
